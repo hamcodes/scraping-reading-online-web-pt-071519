26424e5072dbd90fa06b440e2f0417fcb5989ae6
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-09-11 06:52:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,5 +2,5 @@ require 'nokogiri'
 require 'open-uri'
  
 doc = Nokogiri::HTML(open("http://flatironschool.com/"))
-doc.css(".headline-26OIBN")
+doc.css(".site-header__hero__headline")
 # <h1 class=".headline-26OIBN">Change things.</h1>
